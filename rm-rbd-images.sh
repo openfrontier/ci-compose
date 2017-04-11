a9875bedf085e4e2703fff819cc51dad08a45edd
thinkernel <thinkernel@gmail.com>
2017-04-11 18:54:05 +0800

    Add scripts to remove rbd images

---
+++ rm-rbd-images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rbd rm demo_gerrit-volume
+rbd rm demo_jenkins-volume
+rbd rm demo_nexus-volume
+rbd rm demo_pg-gerrit-volume
+rbd rm demo_pg-redmine-volume
+rbd rm demo_redmine-data-volume
+rbd rm demo_redmine-log-volume
